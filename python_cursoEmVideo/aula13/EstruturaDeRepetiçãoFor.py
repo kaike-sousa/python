db9374bd4e58b895b4b572528cd015a4f1e93138
Kaike Sousa <kaike.cyw@gmail.com>
2025-05-08 15:52:52 -0300

    corrigindo e adicionando jogo joken pon

---
+++ python_cursoEmVideo/aula13/EstruturaDeRepetiçãoFor.py
@@ -0,0 +1,15 @@
+for c in range(0, 6): #vai de 0 a 6, pois não considera o ultimo numero
+    print('oi') 
+print('FIM') #não considera para repetir, pois não está dentro do laço
+
+for c in range(0, 6): #vai de 0 a 6, pois não considera o ultimo numero
+    print('oi') 
+    print('FIM') # considera para repetir, pois está dentro do laço
+
+for c in range(6, 0, -1): #vai de 6 a 0, tirando 1
+    print('oi') 
+print('FIM') #
+
+for c in range (0, 7, 2): #vai de 0 a 7, de 2 em 2
+    print(c)
+print('FIM')
